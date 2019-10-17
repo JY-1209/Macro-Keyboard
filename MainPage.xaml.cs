0c3fcf9bf21bfd90dcd23cc3e4adbb9fd63d382b
Justyn-1209 <53106620+Justyn-1209@users.noreply.github.com>
2019-10-17 00:30:48 -0700

    10/17 (Home Work)
    - Added a new homepage
    -Constructed a basic macro keyboard layout

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,19 +12,44 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace MacroKeyboard
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Macro_Keyboard
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    /// 
+
+    public sealed partial class MainPage : Windows.UI.Xaml.Controls.Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            APODListBoxItem.IsSelected = true;
+            InnerFrame.Navigate(typeof(HomePage));
+            IconsListBox.SelectedItem = APODListBoxItem;
         }
+
+        private void Hamburger_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
